7b849d0be11b2ea77abbe98ccc3a08e946ccfaae
uturkarslan <ugurturkarslan@gmail.com>
2023-02-07 15:44:03 +0100

    Add Terraform fundamentals

---
+++ terraform/host/01_variables.tf
@@ -0,0 +1,26 @@
+#################
+### Variables ###
+#################
+
+### General ###
+
+# New Relic account ID
+variable "NEW_RELIC_ACCOUNT_ID" {
+  type = string
+}
+
+# New Relic API key
+variable "NEW_RELIC_API_KEY" {
+  type = string
+}
+
+# New Relic region
+variable "NEW_RELIC_REGION" {
+  type = string
+}
+
+# Host name
+variable "host_name" {
+  type = string
+}
+######
