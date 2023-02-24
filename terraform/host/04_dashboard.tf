9cc6bc54f499ab66cc8351f67928fb6e29f79807
uturkarslan <ugurturkarslan@gmail.com>
2023-02-24 18:07:10 +0100

    Improve

--- terraform/host/04_dashboard.tf
+++ terraform/host/04_dashboard.tf
@@ -94,10 +94,21 @@ resource "newrelic_one_dashboard" "host" {
   page {
     name = "CPU"
 
+    # Total CPU utilization (%)
+    widget_markdown {
+      title  = "Total CPU utilization (%)"
+      column = 1
+      row    = 1
+      width  = 3
+      height = 3
+
+      text = "Total CPU utilization as a percentage. This is not an actual recorded value; it is an alias that combines percentage data from `cpuSystemPercent`, `cpuUserPercent`, `cpuIoWaitPercent` and `cpuStealPercent`.\n\nThis is calculated as:\n\n`(cpuUserPercent + cpuSystemPercent + cpuIOWaitPercent + cpuStealPercent)`"
+    }
+
     # Max total CPU utilization (%)
     widget_bar {
       title  = "Max total CPU utilization (%)"
-      column = 1
+      column = 4
       row    = 1
       width  = 3
       height = 3
@@ -111,9 +122,9 @@ resource "newrelic_one_dashboard" "host" {
     # Total CPU utilization (%)
     widget_line {
       title  = "Total CPU utilization (%)"
-      column = 4
+      column = 7
       row    = 1
-      width  = 9
+      width  = 6
       height = 3
 
       nrql_query {
@@ -122,10 +133,21 @@ resource "newrelic_one_dashboard" "host" {
       }
     }
 
+    # Idle CPU utilization (%)
+    widget_markdown {
+      title  = "Idle CPU utilization (%)"
+      column = 1
+      row    = 4
+      width  = 3
+      height = 3
+
+      text = "The portion of the current CPU utilization capacity that is idle.\n\nThis is calculated as:\n\n`(100.00 - cpuUserPercent - cpuSystemPercent - cpuIOWaitPercent) / elapsed_time`"
+    }
+
     # Max idle CPU utilization (%)
     widget_bar {
       title  = "Max idle CPU utilization (%)"
-      column = 1
+      column = 4
       row    = 4
       width  = 3
       height = 3
@@ -139,9 +161,9 @@ resource "newrelic_one_dashboard" "host" {
     # Idle CPU utilization (%)
     widget_line {
       title  = "Idle CPU utilization (%)"
-      column = 4
+      column = 7
       row    = 4
-      width  = 9
+      width  = 6
       height = 3
 
       nrql_query {
@@ -150,11 +172,22 @@ resource "newrelic_one_dashboard" "host" {
       }
     }
 
+    # I/O wait CPU utilization (%)
+    widget_markdown {
+      title  = "I/O wait CPU utilization (%)"
+      column = 1
+      row    = 7
+      width  = 3
+      height = 3
+
+      text = "The portion of the current CPU utilization composed only of I/O wait time usage.\n\nThis is calculated as:\n\n`current_sample_io_time - previous_sample_io_time) / elapsed_time`"
+    }
+
     # Max I/O wait CPU utilization (%)
     widget_bar {
       title  = "Max I/O wait CPU utilization (%)"
-      column = 1
-      row    = 4
+      column = 4
+      row    = 7
       width  = 3
       height = 3
 
@@ -167,9 +200,9 @@ resource "newrelic_one_dashboard" "host" {
     # I/O wait CPU utilization (%)
     widget_line {
       title  = "I/O wait CPU utilization (%)"
-      column = 4
-      row    = 4
-      width  = 9
+      column = 7
+      row    = 7
+      width  = 6
       height = 3
 
       nrql_query {
@@ -178,11 +211,22 @@ resource "newrelic_one_dashboard" "host" {
       }
     }
 
+    # Steal CPU utilization (%)
+    widget_markdown {
+      title  = "Steal CPU utilization (%)"
+      column = 1
+      row    = 10
+      width  = 3
+      height = 3
+
+      text = "The portion of time when a virtualized CPU is waiting for the hypervisor to make real CPU time available to it."
+    }
+
     # Max steal CPU utilization (%)
     widget_bar {
       title  = "Max steal CPU utilization (%)"
-      column = 1
-      row    = 7
+      column = 4
+      row    = 10
       width  = 3
       height = 3
 
@@ -195,9 +239,9 @@ resource "newrelic_one_dashboard" "host" {
     # Steal CPU utilization (%)
     widget_line {
       title  = "Steal CPU utilization (%)"
-      column = 4
-      row    = 7
-      width  = 9
+      column = 7
+      row    = 10
+      width  = 6
       height = 3
 
       nrql_query {
@@ -206,11 +250,22 @@ resource "newrelic_one_dashboard" "host" {
       }
     }
 
+    # System CPU utilization (%)
+    widget_markdown {
+      title  = "System CPU utilization (%)"
+      column = 1
+      row    = 13
+      width  = 3
+      height = 3
+
+      text = "The portion of the current CPU utilization composed only of system time usage.\n\nThis is calculated as:\n\n`(current_sample_sys_time - previous_sample_sys_time) / elapsed_time`"
+    }
+
     # Max system CPU utilization (%)
     widget_bar {
       title  = "Max system CPU utilization (%)"
-      column = 1
-      row    = 10
+      column = 4
+      row    = 13
       width  = 3
       height = 3
 
@@ -223,9 +278,9 @@ resource "newrelic_one_dashboard" "host" {
     # System CPU utilization (%)
     widget_line {
       title  = "System CPU utilization (%)"
-      column = 4
-      row    = 10
-      width  = 9
+      column = 7
+      row    = 13
+      width  = 6
       height = 3
 
       nrql_query {
@@ -234,11 +289,22 @@ resource "newrelic_one_dashboard" "host" {
       }
     }
 
+    # User CPU utilization (%)
+    widget_markdown {
+      title  = "User CPU utilization (%)"
+      column = 1
+      row    = 16
+      width  = 3
+      height = 3
+
+      text = "The portion of the current CPU utilization composed only of user time usage.\nThis is calculated as:\n\n`current_sample_user_time - previous_sample_user_time) / elapsed_time`"
+    }
+
     # Max user CPU utilization (%)
     widget_bar {
       title  = "Max user CPU utilization (%)"
-      column = 1
-      row    = 13
+      column = 4
+      row    = 16
       width  = 3
       height = 3
 
@@ -251,9 +317,9 @@ resource "newrelic_one_dashboard" "host" {
     # System CPU utilization (%)
     widget_line {
       title  = "System CPU utilization (%)"
-      column = 4
-      row    = 13
-      width  = 9
+      column = 7
+      row    = 16
+      width  = 6
       height = 3
 
       nrql_query {
@@ -266,11 +332,22 @@ resource "newrelic_one_dashboard" "host" {
   page {
     name = "Memory"
 
+    # MEM utilization (%)
+    widget_markdown {
+      title  = "MEM utilization (%)"
+      column = 1
+      row    = 1
+      width  = 3
+      height = 3
+
+      text = "The portion of available memory that is in use on this server, in percentage."
+    }
+
     # Max MEM utilization (%)
     widget_bar {
       title  = "Max MEM utilization (%)"
-      column = 1
-      row    = 7
+      column = 4
+      row    = 1
       width  = 3
       height = 3
 
@@ -283,14 +360,53 @@ resource "newrelic_one_dashboard" "host" {
     # MEM utilization (%)
     widget_line {
       title  = "MEM utilization (%)"
-      column = 4
-      row    = 7
-      width  = 9
+      column = 7
+      row    = 1
+      width  = 6
       height = 3
 
       nrql_query {
         account_id = var.NEW_RELIC_ACCOUNT_ID
         query      = "FROM Metric SELECT average(host.memoryUsedPercent) FACET host.hostname WHERE host.hostname IN ({{hostnames}}) TIMESERIES"
+      }
+    }
+
+    # MEM free (%)
+    widget_markdown {
+      title  = "MEM free (%)"
+      column = 1
+      row    = 4
+      width  = 3
+      height = 3
+
+      text = "The portion of free memory available to this server, in percentage."
+    }
+
+    # Max MEM free (%)
+    widget_bar {
+      title  = "Max MEM free (%)"
+      column = 4
+      row    = 4
+      width  = 3
+      height = 3
+
+      nrql_query {
+        account_id = var.NEW_RELIC_ACCOUNT_ID
+        query      = "FROM Metric SELECT max(host.memoryFreePercent) AS `max` FACET host.hostname WHERE host.hostname IN ({{hostnames}})"
+      }
+    }
+
+    # MEM free (%)
+    widget_line {
+      title  = "MEM free (%)"
+      column = 7
+      row    = 4
+      width  = 6
+      height = 3
+
+      nrql_query {
+        account_id = var.NEW_RELIC_ACCOUNT_ID
+        query      = "FROM Metric SELECT average(host.memoryFreePercent) FACET host.hostname WHERE host.hostname IN ({{hostnames}}) TIMESERIES"
       }
     }
   }
